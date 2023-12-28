a423df039ee9fd889c4fde8d0a90f5f1a9f91e74
wgsxm <2901199020@qq.com>
2023-12-28 19:23:05 +0800

    update FID

--- utils/metrics.py
+++ utils/metrics.py
@@ -4,6 +4,7 @@ import torchvision
 # FID
 def get_fid_score(real_folder: str, generated_folder: str, batch_size=32, dims=64):
     # both folders should include images files (.png, .jpg, etc)
+    # size, name, format of these images don't have to be same
     if torch.cuda.is_available():
         device = "cuda"
     else:
