dbdcc8d5ac282fa56dee5f602a617a211244fdbb
wgsxm <2901199020@qq.com>
2023-12-28 19:20:16 +0800

    update FID

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,12 @@
 import torch
-import torch.nn.functional as F
-import os
-
+from pytorch_fid import fid_score
+import torchvision
 # FID
+def get_fid_score(real_folder: str, generated_folder: str, batch_size=32, dims=64):
+    # both folders should include images files (.png, .jpg, etc)
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+    fid_value = fid_score.calculate_fid_given_paths([real_folder, generated_folder], batch_size=batch_size, device=device, dims=dims)
+    return fid_value
